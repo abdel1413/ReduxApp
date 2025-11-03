649cf79634a1b41bc1fe37d656675968b10e86c8
aboulayetchakoura <aboulayet63@gmail.com>
2025-11-03 05:35:25 -0500

    redux counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
 import { Counter } from './components/Counter'
-//import { UseContextApi } from './components/CounterContext'
+import { CounterSlice } from './components/redux/counterSlice'
+
 import { UseContextApi } from './components/UseContextApi'
 
 
@@ -9,11 +10,13 @@ function App() {
 
 
   return (
+    <>
     <UseContextApi>
       <h1>Welcome to React Redux</h1>
       < Counter />
     </UseContextApi>
-    
+    <CounterSlice />
+    </>
   )
 }
 
---
+++ src/components/redux/CounterSlice.jsx
@@ -0,0 +1,5 @@
+export const CounterSlice = ()=>{
+    return <>
+    <h1>Redux counter</h1>
+    </>
+}
